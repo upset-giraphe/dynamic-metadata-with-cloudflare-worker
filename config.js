b72ed54113785da97d43ba7ceb7fbc20076f4f72
fbriou <fbriou@gmail.com>
2024-05-29 21:59:32 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const config = {
   domainSource: "https://f69a71f6-9fd8-443b-a040-78beb5d404d4.weweb-preview.io", // Your WeWeb app link
-  metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:8wD10mRd/event-meta", // Link of the endpoint that returns the metadata. /{id} will be added to this path. With id being the last parameter of the dynamic page
+  metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:8wD10mRd/event/35/meta", // Link of the endpoint that returns the metadata. /{id} will be added to this path. With id being the last parameter of the dynamic page
   patterns: {
     dynamicPage: "/event/[^/]+"
   }
