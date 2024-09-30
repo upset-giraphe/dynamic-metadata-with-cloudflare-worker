ac45baeac6c6e39b2a79428ec79ebee3679a9324
upset-giraphe <106740931+upset-giraphe@users.noreply.github.com>
2024-09-30 17:57:57 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ export const config = {
   domainSource: "https://de44a60c-0fc7-4e82-bf89-8d4b1b9e0fc9.weweb-preview.io", // Your WeWeb app preview link
   patterns: [
       {
-          pattern: "/product/[^/]+",
+          pattern: "^/product/[^/]+/?$",
           metaDataEndpoint: "https://lfbxzrylbkxjryzzvftv.supabase.co/rest/v1/rpc/get_product_meta"
       }
   ]
