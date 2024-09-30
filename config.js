cb5c5a8f08da2436d9455381724668a9f58dd7b4
upset-giraphe <106740931+upset-giraphe@users.noreply.github.com>
2024-09-30 19:02:59 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,14 @@
 export const config = {
-  domainSource: "https://your-dev-domain.weweb-preview.io", // Your WeWeb app preview link
+  domainSource: "https://de44a60c-0fc7-4e82-bf89-8d4b1b9e0fc9.weweb-preview.io", // Your WeWeb app preview link
   patterns: [
-    // Pattern for production URLs
     {
       pattern: "^/product/[^/]+/?$",
-      metaDataEndpoint: "https://your-supabase-url.supabase.co/rest/v1/rpc/get_product_meta"
+      metaDataEndpoint: "https://lfbxzrylbkxjryzvftv.supabase.co/rest/v1/rpc/get_product_meta"
     },
     // Pattern for dev URLs
     {
       pattern: "^/ww/cms_data_sets/[^/]+/fetch/?$",
-      metaDataEndpoint: "https://your-supabase-url.supabase.co/rest/v1/rpc/get_product_meta"
+      metaDataEndpoint: "https://lfbxzrylbkxjryzvftv.supabase.co/rest/v1/rpc/get_product_meta"
     }
   ]
 };
